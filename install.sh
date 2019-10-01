f4c7dc07a1192db5254c0ec8d5ac49e32037514f
Alex <40795980+AlexProgrammerDE@users.noreply.github.com>
2019-10-01 16:19:39 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,15 @@
+#!/bin/sh
 
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+	else
+	clear
+  	echo"KeyboardVisualizer Installation"
+  	sudo apt install build-essential gcc make qt5-qmake git -y
+	git clone https://github.com/CalcProgrammer1/KeyboardVisualizer.git
+	cd KeyboardVisualizer
+	qmake -makefile
+	make
+	
+fi
